11def7f0a9b46f6f69159aecf2106b0d99862351
Brice Chichereau <bchicereau@gmail.com>
2022-01-13 16:20:03 +0100

    update direct simulation

--- src/qps/direct.py
+++ src/qps/direct.py
@@ -11,11 +11,11 @@ class Direct(StrongSimulator, WeakSimulator):
     A naive quantum circuit simulator based on matrix-vector multiplication.
     """
 
-    def __init__(self, nqbits):
+    def __init__(self, nqbits: int):
         self.n = nqbits
 
         # Initialize with state |0>
-        self.state = np.zeros(2 ** self.n)
+        self.state = np.zeros(1 << self.n, dtype=np.complex128)
         self.state[0] = 1
 
     def simulate_gate(self, gate, qubits):
@@ -27,23 +27,24 @@ class Direct(StrongSimulator, WeakSimulator):
         # Reorder tensor axes to have modified qubits at the start
         state_tensor = np.moveaxis(state_tensor, qubits, range(k))
 
-        # Reshape state tensor into matrix
-        m = state_tensor.reshape((2 ** k, 2 ** (self.n - k)))
+        # Reshape state tensor into a (2**k, 2**(n-k)) matrix
+        state_tensor = state_tensor.reshape((1 << k, 1 << (self.n - k)))
 
         # Apply gate
-        m = gate @ m
+        state_tensor = gate @ state_tensor
 
-        # Return state tensor to original shape
-        state_tensor = m.reshape([2] * self.n)
+        # Return state tensor to its original shape
+        state_tensor = state_tensor.reshape([2] * self.n)
         state_tensor = np.moveaxis(state_tensor, range(k), qubits)
 
-        self.state = state_tensor.reshape(2 ** self.n)
+        # Update flattened state
+        self.state = state_tensor.reshape(1 << self.n)
 
     def get_probability(self, classical_state):
-        # Return probability of state |<d|C|0>|²
+        # Return probability of state d: |<d|C|0>|²
         return abs(self.state[int(classical_state, 2)]) ** 2
 
     def get_sample(self):
-        # Sample a random possible state with respect to the probability distribution
-        idx = np.random.choice(range(2 ** self.n), p=np.square(np.abs(self.state)))
+        # Sample a random possible state with respect to the state probability distribution
+        idx = np.random.choice(range(1 << self.n), p=np.square(np.abs(self.state)))
         return format(idx, "b")
